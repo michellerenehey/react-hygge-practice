eec5230437746d0a7c4dc9e1f95f4cd981e8250e
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-03 12:59:16 -0800

    started implementing state fin home and connecting to editor

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,23 +1,31 @@
 import '../styles/Editor.css';
 
-export default function Editor() {
+export default function Editor({ light, setLight }) {
   return (
     <div>
-      <label>light label</label>
-      <select>
-        <option>light 1</option>
-        <option>light 2</option>
-      </select>
-      <label>music label</label>
-      <select>
-        <option>music 1</option>
-        <option>music 2</option>
-      </select>
-      <label>book label</label>
-      <select>
-        <option>book 1</option>
-        <option>book 2</option>
-      </select>
+      <div className="top">
+        <label>light label</label>
+        <select>
+          <option>light 1</option>
+          <option>light 2</option>
+        </select>
+      </div>
+
+      <div className="top">
+        <label>music label</label>
+        <select>
+          <option>music 1</option>
+          <option>music 2</option>
+        </select>
+      </div>
+
+      <div className="top">
+        <label>book label</label>
+        <select>
+          <option>book 1</option>
+          <option>book 2</option>
+        </select>
+      </div>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
+
 import Editor from '../Editor/Editor';
 import Preview from '../Preview/Preview';
 
 import '../styles/Home.css';
 
 export default function Home() {
+  const [light, setLight] = useState('');
+
   return (
     <main>
-      <Editor />
+      <Editor {...{ light, setLight }} />
       <Preview />
     </main>
   );
