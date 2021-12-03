f9f98415108389c039c74689b5bedd538cfb4f5c
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-03 12:50:10 -0800

    began editor component

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,5 +1,23 @@
 import '../styles/Editor.css';
 
 export default function Editor() {
-  return <p>hello editor section</p>;
+  return (
+    <div>
+      <label>light label</label>
+      <select>
+        <option>light 1</option>
+        <option>light 2</option>
+      </select>
+      <label>music label</label>
+      <select>
+        <option>music 1</option>
+        <option>music 2</option>
+      </select>
+      <label>book label</label>
+      <select>
+        <option>book 1</option>
+        <option>book 2</option>
+      </select>
+    </div>
+  );
 }
